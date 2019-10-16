8a8d378e3e2cf490cf3f491411441905e1b1a787
CY Fan <chenyu.fan@mavs.uta.edu>
2019-10-16 15:26:12 -0500

    MAR.validate green

--- src/test/MARTest.java
+++ src/test/MARTest.java
@@ -2,8 +2,10 @@ package test;
 
 import static org.junit.Assert.*;
 import facility_maintenance.model.MAR;
+import facility_maintenance.model.Facility;
 import facility_maintenance.model.MARErrorMsgs;
 import facility_maintenance.data.MARsDAO;
+import facility_maintenance.data.FacilitiesDAO;
 
 import org.easymock.EasyMock;
 import org.junit.Before;
@@ -32,20 +34,25 @@ public class MARTest {
 	}
 
 	@Test
-	@PrepareForTest(MARsDAO.class)
+	@PrepareForTest({MARsDAO.class, FacilitiesDAO.class})
 	@FileParameters("TestCaseTable_CSV/MAR.csv")
 	public void testValidate(int testcaseNum, String action, String expectMsg) {
 		action = action.replace("\"", "");
 		expectMsg = expectMsg.replace("\"", "");
 		mockStatic(MARsDAO.class);
+		mockStatic(FacilitiesDAO.class);
 		ArrayList<MAR> mars = new ArrayList<MAR>();
 		mars.add(mar);
 		mar.setIdx("");
 		EasyMock.expect(MARsDAO.getAssigned(mar)).andReturn(mars);
+		Facility emptyFacility = new Facility();
+		emptyFacility.setDuration("1 2");
+		EasyMock.expect(FacilitiesDAO.getDetail("ff")).andReturn(emptyFacility);
 		mar.setDescription("111");
 		mar.setUrgency("notEmpty");
 		mar.setRepairer("");
 		mar.setEstimate("");
+		mar.setFacilityname("ff");
 		replayAll();
 		MARErrorMsgs marErrMsg = new MARErrorMsgs();
 		mar.validate(action, mar, marErrMsg);
