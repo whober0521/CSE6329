3eebaec8cd43e154aafd4b1de0275de80be47868
weichiu <danielchiutw@gmail.com>
2019-11-13 18:21:04 -0600

    Register validation

--- test/facility_maintenance/selenium/UserTest.java
+++ test/facility_maintenance/selenium/UserTest.java
@@ -1,63 +1,111 @@
 package facility_maintenance.selenium;
 
 import java.util.regex.Pattern;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.*;
+import org.junit.runner.RunWith;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+
+@RunWith(JUnitParamsRunner.class)
 public class UserTest {
   private WebDriver driver;
-  private String baseUrl;
+  private String appURL;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
+  public static String sharedUIMapStr;
+  public static Properties prop;
 
   @Before
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "http://192.168.3.129:8080/";
+	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+	/*
+    driver = new ChromeDriver();
+    baseUrl = "http://localhost:225/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    */
+    
+    driver = new ChromeDriver();
+    //baseUrl = "http://www.adactin.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    prop = new Properties();
+    prop.load(new FileInputStream("Configuration/FM_Configuration.properties"));
+    appURL = prop.getProperty("WCAppURL");
+    prop.load(new FileInputStream(prop.getProperty("SharedUIMap")));
   }
 
   @Test
-  public void testUser() throws Exception {
-    driver.get(baseUrl + "/facility_maintenance/");
-    driver.findElement(By.linkText("Sign up")).click();
-    driver.findElement(By.name("username")).clear();
-    driver.findElement(By.name("username")).sendKeys("whober");
-    driver.findElement(By.name("pwd")).clear();
-    driver.findElement(By.name("pwd")).sendKeys("chen");
-    driver.findElement(By.name("utaid")).clear();
-    driver.findElement(By.name("utaid")).sendKeys("1001161980");
+  @FileParameters("./test/facility_maintenance/selenium/UserRegister.csv")
+  public void Registration(int testcaseNum, 
+		  	String username,
+		  	String pwd,
+		  	String role,
+		  	String utaid,
+		  	String firstName,
+		  	String lastName,
+		  	String email,
+		  	String phone,
+		  	String address,
+		  	String city,
+		  	String state) throws Exception {
+	driver.get(appURL);
+    driver.findElement(By.linkText(prop.getProperty("Btn_Login_Registration"))).click();
+    driver.findElement(By.xpath(prop.getProperty("Txt_Registration_Username"))).clear();
+    driver.findElement(By.xpath(prop.getProperty("Txt_Registration_Username"))).sendKeys(username);
+    driver.findElement(By.xpath(prop.getProperty("Txt_Registration_Password"))).clear();
+    driver.findElement(By.xpath(prop.getProperty("Txt_Registration_Password"))).sendKeys(pwd);
+    new Select(driver.findElement(By.id(prop.getProperty("Lst_Registration_Role")))).selectByVisibleText(role);
+    driver.findElement(By.name(prop.getProperty("Lst_Registration_UTAID"))).clear();
+    driver.findElement(By.name(prop.getProperty("Lst_Registration_UTAID"))).sendKeys(utaid);
     driver.findElement(By.name("fname")).clear();
-    driver.findElement(By.name("fname")).sendKeys("whopper");
-    driver.findElement(By.name("username")).clear();
-    driver.findElement(By.name("username")).sendKeys("whopper");
+    driver.findElement(By.name("fname")).sendKeys(firstName);
     driver.findElement(By.name("lname")).clear();
-    driver.findElement(By.name("lname")).sendKeys("chen");
+    driver.findElement(By.name("lname")).sendKeys(lastName);
     driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys("whopper@uta.edu");
+    driver.findElement(By.name("email")).sendKeys(email);
     driver.findElement(By.name("phone")).clear();
-    driver.findElement(By.name("phone")).sendKeys("1234567890");
+    driver.findElement(By.name("phone")).sendKeys(phone);
     driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys("UTA");
+    driver.findElement(By.name("address")).sendKeys(address);
     driver.findElement(By.name("city")).clear();
-    driver.findElement(By.name("city")).sendKeys("DFW");
-    new Select(driver.findElement(By.name("state"))).selectByVisibleText("Texas");
+    driver.findElement(By.name("city")).sendKeys(city);
+    new Select(driver.findElement(By.name("state"))).selectByVisibleText(state);
+    Thread.sleep(1000);
     driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
-    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
-    driver.findElement(By.linkText("Create Problem Report")).click();
-    driver.findElement(By.id("msg")).clear();
-    driver.findElement(By.id("msg")).sendKeys("TEST");
-    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
-    driver.findElement(By.id("msg")).clear();
-    driver.findElement(By.id("msg")).sendKeys("test");
-    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
-    driver.findElement(By.linkText("Logout")).click();
+    Thread.sleep(1000);
+    driver.quit();
+/*    String methodName = new Throwable().getStackTrace()[0].getMethodName();
+    takeScreenshot(driver, "FacilityManagerTest" + methodName + testcaseNum);*/
   }
+
+  public void takeScreenshot(WebDriver driver, String screenshotname) {
+		try
+		{
+			java.io.File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);			
+			FileUtils.copyFile(source, new File("./ScreenShots/" + screenshotname +".png"));
+		}
+		catch(IOException e) {}
+		try {
+			//				  Change the delay value to 1_000 to insert a 1 second delay after 
+			//				  each screenshot
+			Thread.sleep(0);
+		} catch (InterruptedException e) {}
+	}
+  
 
   @After
   public void tearDown() throws Exception {
