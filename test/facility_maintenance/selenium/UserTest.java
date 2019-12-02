9b3f0f8037678b13beb7a2247e440ebed9068b28
Jenny <whober0521@gmail.com>
2019-12-02 09:14:26 -0600

    Merge Shantanu's code

--- test/facility_maintenance/selenium/UserTest.java
+++ test/facility_maintenance/selenium/UserTest.java
@@ -25,18 +25,19 @@ public class UserTest extends facility_maintenance.FMFunctions{
 
   @Before
   public void setUp() throws Exception {
-	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");;
-	options.addArguments("disable-gpu");
-	options.addArguments("--start-fullscreen");
-	options.addArguments("--disable-features=VizDisplayCompositor");
-	
-	driver = new ChromeDriver(options);
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    prop = new Properties();
-    prop.load(new FileInputStream("Configurations/Configuration.properties"));
-    appURL = prop.getProperty("WCAppURL");
-    prop.load(new FileInputStream(prop.getProperty("SharedUIMap")));
+	  System.setProperty("webdriver.chrome.driver","c:/ChromeDriver/chromedriver.exe");
+	  options.addArguments("disable-gpu");
+	  options.addArguments("--start-fullscreen");
+	  options.addArguments("--disable-features=VizDisplayCompositor");
+	  
+	  driver = new ChromeDriver(options);
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  prop = new Properties();
+	  prop.load(new FileInputStream("Configurations/Configuration.properties"));
+	  appURL = prop.getProperty("AppURL");
+	  prop.load(new FileInputStream(prop.getProperty("SharedUIMap")));
   }
+  
   @Test
   @FileParameters("./test/facility_maintenance/selenium/UserRegister.csv")
   public void Registration(int testcaseNum, 
@@ -58,6 +59,25 @@ public class UserTest extends facility_maintenance.FMFunctions{
 	//takeScreenshot(driver, "UserTest" + methodName + testcaseNum);
   }
   
+//  @Test
+//  @FileParameters("./test/facility_maintenance/selenium/UserUpdate.csv")
+//  public void userUpdate(int testcaseNum, 
+//        String pwd,
+//        String utaid,
+//        String firstName,
+//        String lastName,
+//        String email,
+//        String phone,
+//        String address,
+//        String city,
+//    	String state
+//        ) throws Exception {
+//	  driver.get(appURL); 
+//	  String methodName = new Throwable().getStackTrace()[0].getMethodName();
+//	  FM_userUpdate(driver,pwd, utaid, firstName, lastName, email, phone, address, city,state, "UserTest" + methodName + testcaseNum);
+//	  Thread.sleep(1000);
+//  }
+  
   @Test
   @FileParameters("./test/facility_maintenance/selenium/UserLogin.csv")
   public void Login(int testcaseNum, String username, String password ) throws Exception
@@ -74,7 +94,7 @@ public class UserTest extends facility_maintenance.FMFunctions{
   @FileParameters("./test/facility_maintenance/selenium/UserReport.csv")
   public void MarReport(int testcaseNum, String facility, String description) throws Exception {
 		driver.get(appURL);
-		FM_Login(driver, "wei", "!123462");
+		FM_Login(driver, "Sha", "shan");
 		
 	    Thread.sleep(1000);
 	    driver.findElement(By.linkText(prop.getProperty("Txt_ProblemReport"))).click();
@@ -88,28 +108,28 @@ public class UserTest extends facility_maintenance.FMFunctions{
 	    String methodName = new Throwable().getStackTrace()[0].getMethodName();
 	    takeScreenshot(driver, "UserTest" + methodName + testcaseNum);
 }
+  
   @Test
   public void EachLinkCorrect() throws Exception {
-		driver.get(appURL);
-		FM_Login(driver, "wei", "!123462");
-	    
-	    Thread.sleep(1000);
-	    driver.findElement(By.linkText(prop.getProperty("Txt_Home"))).click();
-	    Thread.sleep(1000);
-	    
-	    Thread.sleep(1000);
-	    driver.findElement(By.linkText(prop.getProperty("Txt_Update_Profile"))).click();
-	    Thread.sleep(1000);
-	    driver.navigate().back();
-	    
-	    Thread.sleep(1000);
-	    
-	    FM_Logout(driver);
-	    
-	    Thread.sleep(1000);
+	  driver.get(appURL);
+	  FM_Login(driver, "Sha", "shan");
+	  
+	  Thread.sleep(1000);
+	  driver.findElement(By.linkText(prop.getProperty("Txt_Home"))).click();
+	  Thread.sleep(1000);
+	  
+	  Thread.sleep(1000);
+	  driver.findElement(By.linkText(prop.getProperty("Txt_Update_Profile"))).click();
+	  Thread.sleep(1000);
+	  driver.navigate().back();
+	  
+	  Thread.sleep(1000);
+	  
+	  FM_Logout(driver);
+	  
+	  Thread.sleep(1000);
   }
   
-
   @After
   public void tearDown() throws Exception {
     driver.quit();
